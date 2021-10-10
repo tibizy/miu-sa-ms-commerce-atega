c86777053f33ec2f28415ec0d927e40ba1a5df09
Ayodeji Babatunde <SN027869@firstbanknigeria.com>
2021-10-10 17:40:26 -0500

    initial commit for bank service

---
+++ BankService/src/main/java/edu/miu/sa/bankservice/controllers/PaymentController.java
@@ -0,0 +1,30 @@
+package edu.miu.sa.bankservice.controllers;
+
+import edu.miu.sa.bankservice.dto.BasicResponse;
+import edu.miu.sa.bankservice.dto.PaymentDTO;
+import edu.miu.sa.bankservice.services.BankPaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+    @Autowired
+    BankPaymentService bankPaymentService;
+
+    @RequestMapping(value = "/bank/pay", method = RequestMethod.POST)
+    @Async
+    public ResponseEntity<BasicResponse> cardPayment(@RequestBody PaymentDTO request){
+        BasicResponse response = bankPaymentService.pay(request);
+
+        if(!response.isSuccessful){
+            return new ResponseEntity<BasicResponse>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<BasicResponse>(HttpStatus.OK);
+    }
+}
