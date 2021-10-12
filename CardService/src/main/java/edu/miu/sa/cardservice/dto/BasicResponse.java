ace02cf7b700f00caedec4df8fdffbe5b5df123c
Ayodeji Babatunde <SN027869@firstbanknigeria.com>
2021-10-11 22:30:47 -0500

    commit changes

--- CardService/src/main/java/edu/miu/sa/cardservice/dto/BasicResponse.java
+++ CardService/src/main/java/edu/miu/sa/cardservice/dto/BasicResponse.java
@@ -1,7 +1,33 @@
 package edu.miu.sa.cardservice.dto;
 
 public class BasicResponse {
-    public Boolean isSuccessful;
+    private Boolean isSuccessful;
+    private String responseCode;
+    private String responseDescription;
+
+    public Boolean getSuccessful() {
+        return isSuccessful;
+    }
+
+    public void setSuccessful(Boolean successful) {
+        isSuccessful = successful;
+    }
+
+    public String getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(String responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getResponseDescription() {
+        return responseDescription;
+    }
+
+    public void setResponseDescription(String responseDescription) {
+        this.responseDescription = responseDescription;
+    }
 
     public BasicResponse(){
         isSuccessful = false;
