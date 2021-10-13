57bb88d42a38b086b29b8f3d01f63a0955f0d52d
dj ff <kevinlarry53@gmail.com>
2021-10-13 13:55:44 -0500

    feat: add all

--- orders/src/main/java/com/djff/orders/controllers/OrderController.java
+++ orders/src/main/java/com/djff/orders/controllers/OrderController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import com.djff.orders.services.OrderService;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/api/orders")
 public class OrderController {
 
     final private OrderService orderService;
