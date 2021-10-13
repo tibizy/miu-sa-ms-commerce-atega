e6d7f07cc3f2260c0e44bd272f6cd4a98c79422a
Ayodeji Babatunde <SN027869@firstbanknigeria.com>
2021-10-13 16:33:26 -0500

    commiting changes to payment-service-refactored. Providing a flat paymentDTO

--- PaymentService/src/main/java/edu/miu/sa/paymentservice/dtos/PaymentDTO.java
+++ PaymentService/src/main/java/edu/miu/sa/paymentservice/dtos/PaymentDTO.java
@@ -6,9 +6,95 @@ import edu.miu.sa.paymentservice.model.PaymentType;
 import java.time.LocalDateTime;
 
 public class PaymentDTO {
-    public Long customerReference;
-    public String orderNumber;
-    public Double amount;
-    public PaymentType type;
+    private Long customerReference;
+    private String orderNumber;
+    private Double amount;
+    private PaymentType type;
+    private String cardNumber;
+    private String nameOnCard;
+    private String expDate;
+    private String accountNo;
+    private String routingNo;
+    private String accountName;
+
+    public Long getCustomerReference() {
+        return customerReference;
+    }
+
+    public void setCustomerReference(Long customerReference) {
+        this.customerReference = customerReference;
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public PaymentType getType() {
+        return type;
+    }
+
+    public void setType(PaymentType type) {
+        this.type = type;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getNameOnCard() {
+        return nameOnCard;
+    }
+
+    public void setNameOnCard(String nameOnCard) {
+        this.nameOnCard = nameOnCard;
+    }
+
+    public String getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(String expDate) {
+        this.expDate = expDate;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public String getRoutingNo() {
+        return routingNo;
+    }
+
+    public void setRoutingNo(String routingNo) {
+        this.routingNo = routingNo;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
 }
 
